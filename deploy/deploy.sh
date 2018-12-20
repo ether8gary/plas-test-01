5b867d8a0346de23354abd666c19f453853b489a
ether8gary <ether8gary@gmail.com>
2018-12-20 12:42:31 -0800

    working with automatic docker tags

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,10 +5,10 @@
 #git clone https://github.com/ether8gary/plas-test-01.git
 
 #Build Docker container
-sudo docker build -t tulasi:v1 . && wait
+docker build -t tulasi:v1 . && wait
 
 #Configure container to open port 80 for nginx to server website
-sudo docker run -d -p 80:80 tulasi:v1
+docker run -d -p 80:80 tulasi:v1
 
 #Test that container and website are functioning
 hostname="localhost"
@@ -20,5 +20,5 @@ else
 fi
 
 #Tag docker image with remote respository and push to dockerhub
-container="docker ps -l -q"
-sudo docker tag $container tulasi465/task:v1 && sudo docker push tulasi465/task:v1
+alias tag_container='docker ps -l -q'
+sudo docker tag `tag_container` tulasi465/task:v1 && sudo docker push tulasi465/task:v1
